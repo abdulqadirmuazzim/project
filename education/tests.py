5919d9fb5430f381b92dbcf8f61f533a8d83623e
abdul <abdulqadimuazzim@gmail.com>
2023-02-16 00:15:15 +0100

    done some tests

--- education/tests.py
+++ education/tests.py
@@ -1,22 +1,20 @@
+import sys
+from PyQt5 import Qtwidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 # from django.test import TestCase
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+
+print(sys.argv)
+
+
+def win():
+    app = QApplication(sys.argv)
+    won = QMainWindow()
+    won.show()
+    sys.exit(app.exec_())
+
+
+win()
+
+
 # Create your tests here.
-web = webdriver.Edge()
-
-web.get("file:///C:/Users/King_Abdul/project/education/templates/index.html")
-time.sleep(3)
-var = web.find_element(By.LINK_TEXT, "Products")
-var.click()
-
-time.sleep(3)
-var2 = web.find_element(By.LINK_TEXT, "Home")
-var2.click()
-
-time.sleep(3)
-var3 = web.find_element(By.LINK_TEXT, "Apis")
-var3.click()
-
-time.sleep(3)
-web.quit()
